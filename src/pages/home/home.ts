59f840e50d1b106cef86722ab00a93d6a3a98027
Tobias Kündig <tobias@offline.swiss>
2017-06-10 18:50:52 +0200

    Added @types/leaflet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -67,7 +67,8 @@ export class HomePage {
 
     loadMap () {
         const mymap = Leaflet.map('map').setView([51.505, -0.09], 13);
-        Leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        
+        Leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
             id: config.id,
